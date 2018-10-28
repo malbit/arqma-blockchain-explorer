0d7430d3dffde2cd0e71cbe0a14c8c80f80d9dc7
Michal vel m@lbit <michal@arqma.com>
2018-10-28 03:51:41 +0000

    Revert "."
    This reverts commit 1c499c989df60c5ffce073c65caa4e80d275398e.

--- src/MempoolStatus.cpp
+++ src/MempoolStatus.cpp
@@ -94,6 +94,7 @@ MempoolStatus::start_mempool_status_thread()
 }
 
 
+bool
 MempoolStatus::read_mempool()
 {
     rpccalls rpc {deamon_url};
@@ -125,40 +126,28 @@ MempoolStatus::read_mempool()
         // get transaction info of the tx in the mempool
         const tx_info& _tx_info = mempool_tx_info.at(i);
 
-        crypto::hash mem_tx_hash = null_hash;
+        transaction tx;
+        crypto::hash tx_hash;
+        crypto::hash tx_prefix_hash;
 
-        if (epee::string_tools::hex_to_pod(_tx_info.id_hash, mem_tx_hash))
+        if (!parse_and_validate_tx_from_blob(
+                _tx_info.tx_blob, tx, tx_hash, tx_prefix_hash))
         {
-            transaction tx;
+            cerr << "Cant make tx from tx_blob" << endl;
+            return false;
+        }
 
-            if (!xmreg::make_tx_from_json(_tx_info.tx_json, tx))
-            {
-                cerr << "Cant make tx from _tx_info.tx_json" << endl;
-                return false;
-            }
+        mempool_size_kB += _tx_info.blob_size;
 
-            crypto::hash tx_hash_reconstructed = get_transaction_hash(tx);
+        local_copy_of_mempool_txs.push_back(mempool_tx {tx_hash, tx});
 
-            if (mem_tx_hash != tx_hash_reconstructed)
-            {
-                cerr << "Hash of reconstructed tx from json does not match "
-                        "what we should get!"
-                     << endl;
+        mempool_tx& last_tx = local_copy_of_mempool_txs.back();
 
-                return false;
-            }
+        // key images of inputs
+        vector<txin_to_key> input_key_imgs;
 
-            mempool_size_kB += _tx_info.blob_size;
-
-            local_copy_of_mempool_txs.push_back(mempool_tx {tx_hash_reconstructed, tx});
-
-            mempool_tx& last_tx = local_copy_of_mempool_txs.back();
-
-            // key images of inputs
-            vector<txin_to_key> input_key_imgs;
-
-            // public keys and xmr amount of outputs
-            vector<pair<txout_to_key, uint64_t>> output_pub_keys;
+        // public keys and xmr amount of outputs
+        vector<pair<txout_to_key, uint64_t>> output_pub_keys;
 
         // sum xmr in inputs and ouputs in the given tx
         const array<uint64_t, 4>& sum_data = summary_of_in_out_rct(
